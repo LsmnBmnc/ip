882bc70c0c678f364be3af36eaefe81023ebbbe8
Nanashiwo <108486145+LsmnBmnc@users.noreply.github.com>
2025-01-26 19:55:35 +0800

    Level-0

--- src/main/java/Nana.java
+++ src/main/java/Nana.java
@@ -7,7 +7,7 @@ public class Nana {
 
     public static void processInput (String input, Scanner scanner) throws NanaException {
         if (input.equals("blah")){
-            throw new NanaException("It seems nonsense");
+            throw new NanaException("It seems noMeaning");
         }
         if (input.equals("list")) {
             listTasks();
